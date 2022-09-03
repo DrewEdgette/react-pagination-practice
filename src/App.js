f6f841fa29b0046b936afaad8cf71b11adb5d1d9
Drew <drewedgette@gmail.com>
2022-09-03 16:24:46 -0400

    tried to make pagination a state variable

--- src/App.js
+++ src/App.js
@@ -6,19 +6,14 @@ import UserInfo from "./UserInfo";
 function App() {
   const [userList, setUserList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [pagination, setPagination] = useState([]);
 
   const RESULTS_PER_PAGE = 5;
   const TOTAL_RESULTS = 50;
 
-  const pagination = [];
-
   let lastIndex = currentPage * RESULTS_PER_PAGE;
   let firstIndex = lastIndex - RESULTS_PER_PAGE;
   let slice = userList.slice(firstIndex, lastIndex);
-
-  for (let i = 1; i <= Math.ceil(TOTAL_RESULTS / RESULTS_PER_PAGE); i++) {
-    pagination.push(i);
-  }
 
   const fetchUserList = async () => {
     const response = await fetch(
@@ -29,8 +24,19 @@ function App() {
     setUserList(json.results);
   };
 
+  const fillPagination = () => {
+    const arr = [];
+
+    for (let i = 1; i <= Math.ceil(TOTAL_RESULTS / RESULTS_PER_PAGE); i++) {
+      arr.push(i);
+    }
+
+    setPagination(arr);
+  };
+
   useEffect(() => {
     fetchUserList();
+    fillPagination();
   }, []);
 
   return (
